cdbed8c4621e4911eca15f02901da08020828e09
Jinyu <cadenho@hotmail.com>
2023-12-27 13:34:12 +0800

    init

---
+++ android/app/src/main/java/com/example/cross_page_intro/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.cross_page_intro;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,495 @@
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
+
+import 'interactive_example.dart';
+import 'modal-screen.dart';
+import 'screens.dart';
+
+void main() => runApp(const PersistBottomNavBarDemo());
+
+class PersistBottomNavBarDemo extends StatelessWidget {
+  const PersistBottomNavBarDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Persistent Bottom Navigation Bar Demo',
+      home: const MainMenu(),
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/first': (context) => const MainScreen2(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/second': (context) => const MainScreen3(),
+      },
+    );
+  }
+}
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({Key? key}) : super(key: key);
+
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Navigation Bar Demo"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: ElevatedButton(
+              child: const Text("Custom widget example"),
+              onPressed: () => pushNewScreen(
+                context,
+                screen: const CustomWidgetExample(),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          Center(
+            child: ElevatedButton(
+              child: const Text("Built-in style example"),
+              onPressed: () => pushNewScreen(
+                context,
+                screen: const ProvidedStyleExample(),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          Center(
+            child: ElevatedButton(
+              child: const Text("Interactive Example"),
+              onPressed: () => pushNewScreen(
+                context,
+                screen: InteractiveExample(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// ----------------------- Using a provided Navbar style ---------------------//
+
+class ProvidedStyleExample extends StatefulWidget {
+  const ProvidedStyleExample({Key? key}) : super(key: key);
+
+  @override
+  _ProvidedStyleExampleState createState() => _ProvidedStyleExampleState();
+}
+
+class _ProvidedStyleExampleState extends State<ProvidedStyleExample> {
+  final PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+  bool _hideNavBar = false;
+
+  List<Widget> _buildScreens() {
+    return [
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.home),
+        title: "Home",
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+        inactiveColorSecondary: Colors.purple,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.search),
+        title: "Search",
+        activeColorPrimary: Colors.teal,
+        inactiveColorPrimary: Colors.grey,
+        routeAndNavigatorSettings: RouteAndNavigatorSettings(
+          initialRoute: '/',
+          routes: {
+            '/first': (context) => const MainScreen2(),
+            '/second': (context) => const MainScreen3(),
+          },
+        ),
+      ),
+      PersistentBottomNavBarItem(
+          icon: const Icon(Icons.add),
+          title: "Add",
+          activeColorPrimary: Colors.blueAccent,
+          activeColorSecondary: Colors.white,
+          inactiveColorPrimary: Colors.white,
+          routeAndNavigatorSettings: RouteAndNavigatorSettings(
+            initialRoute: '/',
+            routes: {
+              '/first': (context) => const MainScreen2(),
+              '/second': (context) => const MainScreen3(),
+            },
+          ),
+          onPressed: (context) {
+            if (context != null) {
+              pushDynamicScreen(context,
+                  screen: SampleModalScreen(), withNavBar: true);
+            }
+          }),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.message),
+        title: "Messages",
+        activeColorPrimary: Colors.deepOrange,
+        inactiveColorPrimary: Colors.grey,
+        routeAndNavigatorSettings: RouteAndNavigatorSettings(
+          initialRoute: '/',
+          routes: {
+            '/first': (context) => const MainScreen2(),
+            '/second': (context) => const MainScreen3(),
+          },
+        ),
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.settings),
+        title: "Settings",
+        activeColorPrimary: Colors.indigo,
+        inactiveColorPrimary: Colors.grey,
+        routeAndNavigatorSettings: RouteAndNavigatorSettings(
+          initialRoute: '/',
+          routes: {
+            '/first': (context) => const MainScreen2(),
+            '/second': (context) => const MainScreen3(),
+          },
+        ),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Navigation Bar Demo')),
+      drawer: const Drawer(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('This is the Drawer'),
+            ],
+          ),
+        ),
+      ),
+      body: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        backgroundColor: Colors.white,
+        handleAndroidBackButtonPress: true,
+        resizeToAvoidBottomInset: true,
+        stateManagement: true,
+        navBarHeight: kBottomNavigationBarHeight,
+        hideNavigationBarWhenKeyboardShows: true,
+        margin: const EdgeInsets.all(0.0),
+        popActionScreens: PopActionScreensType.all,
+        bottomScreenMargin: 0.0,
+        onWillPop: (context) async {
+          if (context != null) {
+            await showDialog(
+              context: context,
+              useSafeArea: true,
+              builder: (context) => Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.white,
+                child: ElevatedButton(
+                  child: const Text("Close"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            );
+          }
+          return false;
+        },
+        hideNavigationBar: _hideNavBar,
+        decoration: NavBarDecoration(
+            colorBehindNavBar: Colors.indigo,
+            borderRadius: BorderRadius.circular(20.0)),
+        popAllScreensOnTapOfSelectedTab: true,
+        itemAnimationProperties: const ItemAnimationProperties(
+          duration: Duration(milliseconds: 400),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: const ScreenTransitionAnimation(
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle:
+            NavBarStyle.style17, // Choose the nav bar style with this property
+      ),
+    );
+  }
+}
+
+// ------------------------ Using a custom Navbar style ----------------------//
+
+class CustomWidgetExample extends StatefulWidget {
+  const CustomWidgetExample({Key? key}) : super(key: key);
+
+  @override
+  _CustomWidgetExampleState createState() => _CustomWidgetExampleState();
+}
+
+class _CustomWidgetExampleState extends State<CustomWidgetExample> {
+  final PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+  bool _hideNavBar = false;
+
+  List<Widget> _buildScreens() {
+    return [
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+      MainScreen(
+        hideStatus: _hideNavBar,
+        onScreenHideButtonPressed: () {
+          setState(() {
+            _hideNavBar = !_hideNavBar;
+          });
+        },
+      ),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.home),
+        title: "Home",
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.search),
+        title: ("Search"),
+        activeColorPrimary: Colors.teal,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.add),
+        title: ("Add"),
+        activeColorPrimary: Colors.deepOrange,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.settings),
+        title: ("Settings"),
+        activeColorPrimary: Colors.indigo,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.settings),
+        title: ("Settings"),
+        activeColorPrimary: Colors.indigo,
+        inactiveColorPrimary: Colors.grey,
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Navigation Bar Demo')),
+      drawer: const Drawer(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('This is the Drawer'),
+            ],
+          ),
+        ),
+      ),
+      body: PersistentTabView.custom(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        itemCount: 5,
+        handleAndroidBackButtonPress: true,
+        stateManagement: true,
+        hideNavigationBar: _hideNavBar,
+        screenTransitionAnimation: const ScreenTransitionAnimation(
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        customWidget: (navBarEssentials) => CustomNavBarWidget(
+          items: _navBarsItems(),
+          onItemSelected: (index) {
+            setState(() {
+              navBarEssentials.onItemSelected?.call(index);
+            });
+          },
+          selectedIndex: _controller.index,
+        ),
+      ),
+    );
+  }
+}
+
+class CustomNavBarWidget extends StatelessWidget {
+  final int selectedIndex;
+  final List<PersistentBottomNavBarItem> items;
+  final ValueChanged<int>? onItemSelected;
+
+  const CustomNavBarWidget({
+    super.key,
+    required this.selectedIndex,
+    required this.items,
+    this.onItemSelected,
+  });
+
+  Widget _buildItem(PersistentBottomNavBarItem item, bool isSelected) {
+    return Container(
+      alignment: Alignment.center,
+      height: kBottomNavigationBarHeight,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Flexible(
+            child: IconTheme(
+              data: IconThemeData(
+                  size: 26.0,
+                  color: isSelected
+                      ? (item.activeColorSecondary ?? item.activeColorPrimary)
+                      : item.inactiveColorPrimary ?? item.activeColorPrimary),
+              child: isSelected ? item.icon : item.inactiveIcon ?? item.icon,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: Material(
+              type: MaterialType.transparency,
+              child: FittedBox(
+                child: Text(
+                  item.title ?? "",
+                  style: TextStyle(
+                      color: isSelected
+                          ? (item.activeColorSecondary ??
+                              item.activeColorPrimary)
+                          : item.inactiveColorPrimary,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: SizedBox(
+        width: double.infinity,
+        height: kBottomNavigationBarHeight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: items.map((item) {
+            int index = items.indexOf(item);
+            return Expanded(
+              child: InkWell(
+                onTap: () {
+                  onItemSelected?.call(index);
+                },
+                child: _buildItem(item, selectedIndex == index),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
